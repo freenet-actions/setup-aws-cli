d9eb916444c921bfa0bd4e8e1c4f861a2dffb0d3
mtbui01 <Minhtri.Bui@md.de>
2021-11-18 16:53:44 +0100

    file updated

--- src/app-installer.ts
+++ src/app-installer.ts
@@ -2,7 +2,6 @@ import * as core from '@actions/core'
 import * as exec from '@actions/exec'
 import * as fs from 'fs'
 import * as io from '@actions/io'
-import * as os from 'os'
 import * as path from 'path'
 import * as tc from '@actions/tool-cache'
 
@@ -10,20 +9,15 @@ const toolName = 'aws-cli'
 
 export async function install(version: string): Promise<void> {
   let toolPath: string
-  toolPath = await isAlreadyInstalled()
+  toolPath = tc.find(toolName, version)
   if (!toolPath) {
     toolPath = await downloadAWSCli(version)
   }
-  console.log('tool path', toolPath)
   core.addPath(toolPath)
 }
 
-async function isAlreadyInstalled(): Promise<string> {
-  return tc.find(toolName, '*')
-}
-
-function getDownloadUrl(): string {
-  return 'https://s3.amazonaws.com/aws-cli/awscli-bundle.zip'
+function getDownloadUrl(version: string): string {
+  return `https://awscli.amazonaws.com/awscli-exe-linux-x86_64-${version}.zip`
 }
 
 async function downloadFile(downloadUrl: string): Promise<string> {
@@ -35,29 +29,26 @@ async function downloadFile(downloadUrl: string): Promise<string> {
   return destPath
 }
 
-async function readFile(filepath: string): Promise<string> {
-  try {
-    return fs.readFileSync(filepath, 'utf-8')
-  } catch (err) {
-    throw err
-  }
-}
-
 async function downloadAWSCli(version: string): Promise<string> {
   try {
-    const downloadUrl = getDownloadUrl()
+    const downloadUrl = getDownloadUrl(version)
     let installFilePath = await downloadFile(downloadUrl)
 
     if (path.extname(downloadUrl) === '.zip') {
       const extractedPath = await tc.extractZip(installFilePath)
-      installFilePath = path.join(extractedPath, 'awscli-bundle', 'install')
+      installFilePath = path.join(extractedPath, 'aws', 'install')
 
       const rootPath = path.parse(installFilePath).dir
       const installDestinationDir = path.join(rootPath, '.local', 'lib', 'aws')
-      const installArgs: string[] = ['-i', installDestinationDir]
-      const binFile = 'aws'
+      const binDestinationDir = path.join(rootPath, '.local', 'bin')
+      const installArgs: string[] = [
+        '--install-dir',
+        installDestinationDir,
+        '--bin-dir',
+        binDestinationDir
+      ]
       await exec.exec(installFilePath, installArgs)
-      return path.join(installDestinationDir, 'bin', binFile)
+      return binDestinationDir
     } else {
       const errormsg = 'Unsupported extension'
       throw new Error(errormsg)
