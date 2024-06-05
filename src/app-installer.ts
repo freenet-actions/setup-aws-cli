0450b684e5dbe2dec048a118eca941ebd1e83efd
Lea Reimann <lea.reimann@freenet.ag>
2024-06-05 07:09:33 +0200

    Remove unused dependency (#535)
    Co-authored-by: Lea Reimann <lkreimann@users.noreply.github.com>

--- src/app-installer.ts
+++ src/app-installer.ts
@@ -1,6 +1,5 @@
 import * as core from '@actions/core'
 import * as exec from '@actions/exec'
-import * as fs from 'fs'
 import * as io from '@actions/io'
 import * as path from 'path'
 import * as tc from '@actions/tool-cache'
